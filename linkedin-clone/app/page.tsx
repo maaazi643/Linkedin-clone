ba6749d9994a03cc7f7e621b9f79b77cc2bd40d4
maaazi643 <adeyemimuaz101@gmail.com>
2024-08-17 00:00:50 +0100

    styling added to the post section

--- linkedin-clone/app/page.tsx
+++ linkedin-clone/app/page.tsx
@@ -1,17 +1,20 @@
-import { Button } from "@/components/ui/button";
+import PostForm from "@/components/PostForm";
+
 import UserInformation from "@/components/UserInformation";
 
 export default function Home() {
   return (
-    <div className="grid">
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
       {/* Post section */}
-      <section>
+      <section className="hidden md:inline md:col-span-2">
         <UserInformation />
       </section>
       {/* Post Form and Feed */}
-      <section></section>
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+        <PostForm />
+      </section>
       {/* Widget */}
-      <section></section>
+      <section className="hidden xl:inline justify-center col-span-2"></section>
     </div>
   );
 }
