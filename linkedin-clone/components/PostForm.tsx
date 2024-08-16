7e6fa372866f1557985124cef56ea462319f12c3
maaazi643 <adeyemimuaz101@gmail.com>
2024-08-17 00:15:45 +0100

    added functionality to the postForm

--- linkedin-clone/components/PostForm.tsx
+++ linkedin-clone/components/PostForm.tsx
@@ -2,12 +2,18 @@
 
 import { useUser } from "@clerk/nextjs";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+import { ImageIcon } from "lucide-react";
+import { useRef, useState } from "react";
 
 function PostForm() {
+  const ref = useRef<HTMLFormElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const { user } = useUser();
+  const [preview, setPreview] = useState(false);
   return (
     <div>
-      <form action="">
+      <form action="" ref={ref}>
         <div className=" flex items-center space-x-2">
           <Avatar>
             {user?.id ? (
@@ -22,11 +28,28 @@ function PostForm() {
           </Avatar>
 
           <input
+            ref={fileInputRef}
             type="text"
             name="postInput"
             placeholder="Start writing a post.."
             className="flex-1 outline-none rounded-full py-3 px-4 border"
           />
+
+          <input type="file" name="image" accept="image/*" hidden />
+
+          <button type="submit" hidden>
+            Post
+          </button>
+        </div>
+
+        {/* Preview Conditional Check */}
+        <div>
+          <Button>
+            <ImageIcon className="mr-2" size={16} color="currentColor" />
+            Add Image
+          </Button>
+
+          {/* Add a remove preview button */}
         </div>
       </form>
     </div>
