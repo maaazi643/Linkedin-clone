29da050ea1a69e8c70c1a218a2b4d22ebe52f791
maaazi643 <adeyemimuaz101@gmail.com>
2024-08-19 17:17:45 +0100

    fixed mongoDB error

--- linkedin-clone/mongodb/db.ts
+++ linkedin-clone/mongodb/db.ts
@@ -1,1 +1,20 @@
-const connectionString = `mongodb+srv://${process.env.MONDO_DB_USERNAME}: ${process.env.MONGO_DB_PASSWORD} @linked-clone-3.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000`;
+import mongoose from "mongoose";
+
+const connectionString = `mongodb+srv://${process.env.MONDO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@linked-clone-4.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000`;
+
+if (!connectionString) {
+  throw new Error("Please provide a valid connection string");
+}
+
+const connectDB = async () => {
+  if (mongoose.connection?.readyState >= 1) {
+    return;
+  }
+
+  try {
+    console.log("----- connecting to MONGODB -----");
+    await mongoose.connect(connectionString);
+  } catch (err) {
+    console.log("Error connecting to MONGODB: ", err);
+  }
+};
